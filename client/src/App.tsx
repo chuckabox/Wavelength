2b1446d0072052623534ab281ce1d01531375097
Connor <s4800777@student.uq.edu.au>
2026-07-12 06:05:29 +1000

    fix(client): remove unused useEffect import to fix TS build

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import Header from './components/Header';
 import LiveView from './components/LiveView';
